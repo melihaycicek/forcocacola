638ba0b144eb98a00b672e79495fc4f2b0bc1fb5
melihaycicek <aycckmlh@gmail.com>
2022-09-23 12:43:00 +0300

    trial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./App.css";
+import { Section } from './components/Section/Section';
+import SmoothScroll  from './components/SmoothScroll/SmoothScroll';
+
+function App() {
+  return (
+    <SmoothScroll>
+      <h2>Smooth Scrolling</h2>
+      <Section flexDirection="row" />
+      <Section flexDirection="row-reverse" />
+      <Section flexDirection="row" />
+      <Section flexDirection="row-reverse" />
+      <Section flexDirection="row" />
+      <Section flexDirection="row-reverse" />
+    </SmoothScroll>
+  );
+}
+
+export default App;
